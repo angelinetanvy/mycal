c0a6cb43154196cc61c7bc37e9fec2291bf91a5b
Angeline Tanvy <72086059+angelinetanvy@users.noreply.github.com>
2023-01-31 01:42:29 +0800

    created month page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:uuid/uuid.dart';
 import 'package:http/http.dart' as http;
 import 'package:universal_io/io.dart';
+import 'month.dart';
 
 class MyHttpOverrides extends HttpOverrides{
   @override
@@ -23,114 +23,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mycal',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  var uuid = Uuid();
-
-  void createEvent(){
-    var url = "http://127.0.0.1/mycal/create_event.php";
-    http.post(Uri.parse(url),body: {
-      "id": "2222222222222222",
-      "title": "go to sleep",
-      "start": "1974-03-20 12:00:00",
-      "end": "2022-03-20 12:00:00",
-      "location": "bedroom",
-      "detail": "8 hours",
-    });
-  }
-
-  void updateEvent(){
-    var url = "http://127.0.0.1/mycal/update_event.php";
-    http.post(Uri.parse(url),body: {
-      "id": "2222222222222222",
-      "title": "go to gym",
-      "start": "1974-03-20 12:00:00",
-      "end": "2022-03-20 12:00:00",
-      "location": "gym",
-      "detail": "2 hours",
-    });
-  }
-
-  void deleteEvent(){
-    var url = "http://127.0.0.1/mycal/delete_event.php";
-    http.post(Uri.parse(url),body: {
-      "id": "2222222222222222",
-    });
-  }
-
-  Future<List> getEvent() async{
-    final params = {
-      "id": "2222222222222222",
-    };
-    final uri = Uri.https("127.0.0.1","/mycal/get_event.php", params);
-
-    try {
-      final res = await http.get(uri);  
-      // print(jsonDecode(res.body.substring(16)));
-      return jsonDecode(res.body.substring(16));
-    } catch (e) {
-      print(e);
-      return [];
-    }  
-    
-  }
-
-  Future<List> getEvents() async{
-    final params = {
-      "date": "1974-03-20",
-    };
-    
-    final uri = Uri.https("127.0.0.1","/mycal/get_events.php", params);
-    try {
-        final res = await http.get(uri);  
-        print(jsonDecode(res.body.substring(10)));
-        return jsonDecode(res.body.substring(10));
-    } catch (e) {
-      print(e);
-      return [];
-    }  
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF343A40),
+          secondary: const Color(0xFF343A40),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => getEvents(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: const MonthPage(),
     );
   }
 }
---
+++ lib/month.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:uuid/uuid.dart';
+import 'package:http/http.dart' as http;
+import 'package:universal_io/io.dart';
+
+class MonthPage extends StatefulWidget {
+  const MonthPage({super.key});
+
+  @override
+  State<MonthPage> createState() => _MonthPage();
+}
+
+class _MonthPage extends State<MonthPage> {
+
+  Future<List> getEvents() async{
+    final params = {
+      "date": "1974-03-20",
+    };
+    
+    final uri = Uri.https("127.0.0.1","/mycal/get_events.php", params);
+    try {
+        final res = await http.get(uri);  
+        print(jsonDecode(res.body.substring(10)));
+        return jsonDecode(res.body.substring(10));
+    } catch (e) {
+      print(e);
+      return [];
+    }  
+
+  }
+
+  Widget titleSection = Container(
+    color:Colors.grey[900],
+    height: 170,
+    padding: const EdgeInsets.all(35),
+    child: Column(
+        children:[
+            Container(
+                height:70
+            ),
+            Container(
+                child:
+                    Row(
+                        children:[
+                            Expanded(
+                                child: Text("January 2023",style: TextStyle(fontSize: 25, color:Colors.white),)
+                            ),
+                            Icon(
+                                Icons.arrow_back_ios,
+                                color: Colors.white,
+                            ),
+                            Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.white,
+                            ),
+                        ]
+                    )
+            )
+        ]
+    )
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        body: Column(
+            children:[
+                titleSection,
+            ]
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () => getEvents(),
+            child: const Icon(Icons.add),
+        ),
+    );
+  }
+}
