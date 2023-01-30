38934d1d42e5ee700c426939e397f90390ddeb0a
Angeline Tanvy <72086059+angelinetanvy@users.noreply.github.com>
2023-01-31 06:00:25 +0800

    navigation arrow working

--- lib/month.dart
+++ lib/month.dart
@@ -4,6 +4,7 @@ import 'package:uuid/uuid.dart';
 import 'package:http/http.dart' as http;
 import 'package:universal_io/io.dart';
 import 'new_event.dart';
+import 'package:intl/intl.dart';
 
 class MonthPage extends StatefulWidget {
   const MonthPage({super.key});
@@ -13,6 +14,17 @@ class MonthPage extends StatefulWidget {
 }
 
 class _MonthPage extends State<MonthPage> {
+    DateTime currDate = DateTime.now();
+
+    void nextMonth() {
+        var daysMonth = DateTime(currDate.year, currDate.month + 1, 0).day - currDate.day + 1;
+        setState((){ currDate = currDate.add(Duration(days:daysMonth));});
+    }
+
+    void previousMonth() {
+        var daysMonth = DateTime(currDate.year, currDate.month, 0).day + currDate.day - 1;
+        setState((){currDate = currDate.subtract(Duration(days:daysMonth));});
+    }
 
   Future<List> getEvents() async{
     final params = {
@@ -31,34 +43,62 @@ class _MonthPage extends State<MonthPage> {
 
   }
 
-  Widget titleSection = Container(
-    color:Colors.grey[900],
-    height: 170,
-    padding: const EdgeInsets.all(35),
-    child: Column(
-        children:[
-            Container(
-                height:70
-            ),
-            Container(
-                child:
-                    Row(
-                        children:[
-                            Expanded(
-                                child: Text("January 2023",style: TextStyle(fontSize: 25, color:Colors.white),)
-                            ),
-                            Icon(
-                                Icons.arrow_back_ios,
-                                color: Colors.white,
-                            ),
-                            Icon(
-                                Icons.arrow_forward_ios,
-                                color: Colors.white,
-                            ),
-                        ]
-                    )
-            )
-        ]
+  Widget titleSection() {
+    final DateFormat formatter = DateFormat('yMMMM');
+    var newDate = formatter.format(currDate);
+    return Container(
+        color:Colors.grey[900],
+        height: 170,
+        padding: const EdgeInsets.only(left:35,right:35,bottom:10),
+        child: Column(
+            children:[
+                Container(
+                    height:100
+                ),
+                Container(
+                    child:
+                        Row(
+                            children:[
+                                Expanded(
+                                    child: Text(newDate,style: TextStyle(fontSize: 25, color:Colors.white),)
+                                ),
+                                IconButton(
+                                    icon: Icon(Icons.arrow_back_ios),
+                                    color: Colors.white,
+                                    onPressed: (){ previousMonth();}
+                                ),
+                                IconButton(
+                                    icon: Icon(Icons.arrow_forward_ios),
+                                    color: Colors.white,
+                                    onPressed: (){ nextMonth();}
+                                ),
+                            ]
+                        )
+                )
+            ]
+        )
+    );
+  }
+
+  Widget grid = SizedBox(
+    height:300,
+    child:ListView(
+      children: const <Widget>[
+        Card(child: ListTile(title: Text('One-line ListTile'))),
+        Card(
+          child: ListTile(
+            leading: FlutterLogo(),
+            title: Text('One-line with leading widget'),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text('One-line with trailing widget'),
+            trailing: Icon(Icons.more_vert),
+          ),
+        ),
+
+      ],
     )
   );
 
@@ -68,7 +108,8 @@ class _MonthPage extends State<MonthPage> {
         backgroundColor: Colors.black,
         body: Column(
             children:[
-                titleSection,
+                titleSection(),
+                grid
             ]
         ),
         floatingActionButton: FloatingActionButton(
